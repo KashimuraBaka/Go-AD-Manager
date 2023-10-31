2f229f43b708858907757cac587f898f847c64c6
Kashimura <kashimura@qq.com>
2023-10-31 10:54:46 +0800

    Update：数据返回类型

---
+++ global/request.go
@@ -0,0 +1,28 @@
+package global
+
+const (
+	RES_SUCCESS = 200
+	RES_ERROR   = 201
+)
+
+type Response struct {
+	Code int    `json:"code"`
+	Data any    `json:"data"`
+	Msg  string `json:"msg"`
+}
+
+func ResponseError(msg string) *Response {
+	return &Response{
+		Code: RES_ERROR,
+		Data: nil,
+		Msg:  msg,
+	}
+}
+
+func ResponseSuccess(data any) *Response {
+	return &Response{
+		Code: RES_SUCCESS,
+		Data: data,
+		Msg:  "success",
+	}
+}
