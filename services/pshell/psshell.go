38b20ac00558e20a74e8ce98191ff0692b60013b
Kashimura <kashimura@qq.com>
2023-11-01 10:19:08 +0800

    Update: Powershell错误改进

--- services/pshell/psshell.go
+++ services/pshell/psshell.go
@@ -1,6 +1,13 @@
 package pshell
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io"
+
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+)
 
 var Shell *ADControl
 
@@ -11,10 +18,11 @@ func init() {
 	}
 
 	if err = ps.CreateSession("192.168.102.254", "Administrator", "RGrr2019"); err != nil {
-		panic(err)
+		b := []byte(err.Error())
+		reader := transform.NewReader(bytes.NewReader(b), simplifiedchinese.GBK.NewDecoder())
+		d, _ := io.ReadAll(reader)
+		fmt.Println("[Powershell] Error", string(d))
 	}
-
-	ps.GetUsers("")
 
 	Shell = ps
 }
